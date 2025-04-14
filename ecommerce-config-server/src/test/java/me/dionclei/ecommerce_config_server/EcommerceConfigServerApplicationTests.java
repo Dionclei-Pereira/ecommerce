2e03367c87d58ea7b9f464a6053e53d7647d53e9
Dionclei de Souza Pereira <122059269+Dionclei-Pereira@users.noreply.github.com>
2025-04-14 11:03:48 -0300

    Initial commit

---
+++ ecommerce-config-server/src/test/java/me/dionclei/ecommerce_config_server/EcommerceConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dionclei.ecommerce_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
