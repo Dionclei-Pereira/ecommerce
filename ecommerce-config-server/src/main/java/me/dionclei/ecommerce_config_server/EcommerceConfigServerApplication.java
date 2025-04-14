cd82170dc5ca17fac44268f6cc7f1cf8b304b004
Dionclei de Souza Pereira <122059269+Dionclei-Pereira@users.noreply.github.com>
2025-04-14 11:36:05 -0300

    discovery server

--- ecommerce-config-server/src/main/java/me/dionclei/ecommerce_config_server/EcommerceConfigServerApplication.java
+++ ecommerce-config-server/src/main/java/me/dionclei/ecommerce_config_server/EcommerceConfigServerApplication.java
@@ -2,8 +2,10 @@ package me.dionclei.ecommerce_config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class EcommerceConfigServerApplication {
 
 	public static void main(String[] args) {
---
+++ ecommerce-discovery-server/src/test/java/me/dionclei/ecommerce_discovery_server/EcommerceDiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dionclei.ecommerce_discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceDiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
